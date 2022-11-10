75a54168c12889484077481e95e2fed50cccce4c
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-11-10 13:45:15 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,6 @@
-#Web stack debugging 4
+#Web stack debugging4
+# Changing the ngnix config to repair requests
+exec { 'fix--for-nginx':
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf; sudo service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
